81388ecdbc522baa0e8d1f91239398c483fb107e
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2022-03-25 23:25:57 +0100

    update config

--- config.js
+++ config.js
@@ -2,11 +2,14 @@ import { cpus } from "os";
 
 const config = {
   // Number of workers, intially determined by number of cpus
-  //   NUMBER_OF_WORKERS: cpus().length,
-  NUMBER_OF_WORKERS: 4,
+  NUMBER_OF_WORKERS: cpus().length,
   // min and max delay in miliseconds
   MIN_DELAY: 1000,
   MAX_DELAY: 5000,
+  // limit queue to max 1000 tasks in 5 seconds
+  QUEUE_SIZE: 1000,
+  TIME_LIMIT: 5000,
+  DEFAULT_ATTEMPTS: 3,
 };
 
 export default config;
