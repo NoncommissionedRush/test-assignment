ef4027fa44597804d4864f7b15049896e9a05916
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2022-06-04 21:47:16 +0200

    fix processor

--- server/queues/processor.js
+++ server/queues/processor.js
@@ -1,14 +1,12 @@
 import randomNumber from "../services/random-number.js";
 import config from "../config.js";
 
-const processor = async (job) => {
-  const start = Date.now();
-  // random delay between 1000 and 5000 milis
-  while (
-    Date.now() - start <
-    randomNumber(config.MIN_DELAY, config.MAX_DELAY)
-  ) {}
-  return true;
+const processor = async (_job) => {
+ return new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      resolve(true);
+    }, randomNumber(config.MIN_DELAY, config.MAX_DELAY));
+  });
 };
 
 export default processor;
